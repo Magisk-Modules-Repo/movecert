67d553d3de7f18d18a903212080fb6ca8e81ad00
Mark Wagie <yochanan.marqos@gmail.com>
2018-11-13 18:36:29 -0700

    removed extra file

--- gitgo.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-if [ -z "$1" ];
-    then echo "Please supply a commit message wrapped in single quotes '' before you proceed!";
-    else
-        git add *
-        git commit -am "$1"
-        git push;
-fi
